9fd5206bb3240ca5482e249cc735896d8296af70
Arjun Singh <arjunkhakh@gmail.com>
2022-01-09 14:18:44 +0000

    starting up the webpages and putting the button and question in ready for js

---
+++ assets/javascript.js
@@ -0,0 +1,7 @@
+var button = document.querySelector("#buttonGame");
+
+button.addEventListener("click", function(event) {
+    var quiz1 = document.querySelector("#quiz1")
+
+    document.quiz1.innerHTML = "<pre>" + "Hello!" + "</pre>";
+});
