5e04e09878ab48bd1fba2cd81927394a058b71e8
Arjun Singh <arjunkhakh@gmail.com>
2022-01-13 16:46:10 +0000

    put in my question, answers and added a timer while also styling and adding the id elements to the html page

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,84 @@
 var button = document.querySelector("#buttonGame");
 
-button.addEventListener("click", function(event) {
-    var quiz1 = document.querySelector("#quiz1")
+var quiz1 = document.querySelector("#quiz1")
 
-    document.quiz1.innerHTML = "<pre>" + "Hello!" + "</pre>";
+var timeEl = document.querySelector("#timer")
+
+var scoreEl = document.querySelector("#score")
+
+var questions = [
+{
+    question: "What Tag is used for a hyperlink?",
+    answers: ["<a>", "<li>", "<p>", "<hyperlink>"],
+    correctAnswerIndex: 0,
+},
+{
+    question: "What is the best car company?",
+    answers: ["Tesla","Tesla","Tesla", "Tesla"],
+    correctAnswerIndex: 2,
+},
+];
+
+var secondsLeft = 90;
+
+function score(event) {
+    event.preventDefault();
+
+    scoreEl.textContent = "Please put in initials";
+}
+
+function setTime(event) {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left.";
+
+    if(secondsLeft === 0) {
+        clearInterval(timerInterval)
+        quiz1.style.display = "none";
+        score();
+    }
+
+  }, 1000);
+}
+
+button.addEventListener("click", function(event) {
+    var firstQuestion = questions [0];
+    quiz1.textContent = firstQuestion.question;
+    quiz1.setAttribute("data-index", firstQuestion.correctAnswerIndex);
+
+    for (var i = 0; i < firstQuestion.answers.length; i++) {
+        var answer = firstQuestion.answers[i];
+        var answerEl = document.createElement("li");
+        var buttonEl = document.createElement("button");
+
+        buttonEl.textContent = answer;
+        buttonEl.setAttribute("data-index", i);
+
+        answerEl.append(buttonEl);
+        quiz1.append(answerEl);
+
+        buttonEl.setAttribute("style", "background-color:pink; font-size:40px; margin-top:10px; padding-left:40px; padding-right:40px")
+        answerEl.setAttribute("style", "list-style:none");
+        quiz1.setAttribute("style", "margin:20px")
+    }
+    setTime()
+}
+)
+
+quiz1.addEventListener("click", function (event) {
+    var elementClicked = event.target;
+
+        if (elementClicked.matches("button")) {
+
+        var rightAnswerIndex = quiz1.getAttribute("data-index");
+        var selectedAnswerIndex = elementClicked.getAttribute("data-index");
+
+        if (rightAnswerIndex === selectedAnswerIndex) {
+            alert("You selected the right answer!");
+    } else {
+        alert("You have selected the wrong one!")
+    }
+}
 });
+
